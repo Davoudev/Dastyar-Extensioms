455157f2d16a503d26f60f6072266b365a82867d
david <davidchaharmehali@gmail.com>
2024-07-15 16:06:43 +0430

    add a persian calender

--- src/pages/Calender.jsx
+++ src/pages/Calender.jsx
@@ -1,52 +1,10 @@
 import { Flex, GridItem } from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
-import Days from "../components/calender/days";
+import "react-modern-calendar-datepicker/lib/DatePicker.css";
+import { Calendar } from "react-modern-calendar-datepicker";
 
 const Calender = ({ gridNumber }) => {
-  const months = [
-    "فروردین",
-    "اردیبهشت",
-    "خرداد",
-    "تیر",
-    "مرداد",
-    "شهریور",
-    "مهر",
-    "آبان",
-    "آذر",
-    "دی",
-    "بهمن",
-    "اسفند",
-  ];
-
-  const [showedDate, setShowDate] = useState({
-    year: null,
-    monthName: null,
-    monthDays: null,
-    today: null,
-  });
-
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch("https://api.keybit.ir/time/");
-        if (response.ok) {
-          const result = await response.json();
-          setShowDate({
-            year: result.date.year.number.en,
-            monthName: result.date.month.name,
-            monthDays: result.date.month.number.en,
-            today: result.date.day.number.en,
-          });
-        } else {
-          console.error("Error fetching data:", response.status);
-        }
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
-
-    fetchData();
-  }, []);
+  const [selectedDay, setSelectedDay] = useState(null);
 
   return (
     <GridItem
@@ -57,11 +15,61 @@ const Calender = ({ gridNumber }) => {
       bg={"#13151C"}
       borderRadius={25}
     >
-      {/* <Flex>
-        <Days monthNumber={Number(showedDate.monthDays)} />
-      </Flex> */}
+      <Calendar
+        locale={"fa"}
+        value={selectedDay}
+        onChange={setSelectedDay}
+        calendarClassName="custom-calendar"
+        // calendarTodayClassName={"x"}
+        shouldHighlightWeekends
+      />
     </GridItem>
   );
 };
 
 export default Calender;
+
+// const months = [
+//   "فروردین",
+//   "اردیبهشت",
+//   "خرداد",
+//   "تیر",
+//   "مرداد",
+//   "شهریور",
+//   "مهر",
+//   "آبان",
+//   "آذر",
+//   "دی",
+//   "بهمن",
+//   "اسفند",
+// ];
+
+// const [showedDate, setShowDate] = useState({
+//   year: null,
+//   monthName: null,
+//   monthDays: null,
+//   today: null,
+// });
+
+// useEffect(() => {
+//   const fetchData = async () => {
+//     try {
+//       const response = await fetch("https://api.keybit.ir/time/");
+//       if (response.ok) {
+//         const result = await response.json();
+//         setShowDate({
+//           year: result.date.year.number.en,
+//           monthName: result.date.month.name,
+//           monthDays: result.date.month.number.en,
+//           today: result.date.day.number.en,
+//         });
+//       } else {
+//         console.error("Error fetching data:", response.status);
+//       }
+//     } catch (error) {
+//       console.error("Error fetching data:", error);
+//     }
+//   };
+
+//   fetchData();
+// }, []);
